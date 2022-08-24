a1fd0f37975276b6404ec513d9461a47b0b9321d
yachanMeng <349253282@qq.com>
2022-08-24 14:27:24 +0800

    add

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const  proxy = require('express-http-proxy');
+const fs = require('fs');
+const path = require('path');
 const app = express();
+// const html = fs.readFileSync(path.join(__dirname,'./dist/index.html'));
 app.use(express.static('dist'));
 app.use('/roxe-merchant-cbdc', proxy("http://roxe-gateway-sandbox.roxe.pro",{
   proxyReqPathResolver:(req) => {
@@ -9,6 +12,6 @@ app.use('/roxe-merchant-cbdc', proxy("http://roxe-gateway-sandbox.roxe.pro",{
 }));
 app.all("*",(req,res) => {
   console.log(`path:${req.path}`)
-  res.status(404).send("not found");
+  res.sendFile(path.join(__dirname,'./dist/index.html'))
 })
 app.listen(4200);
